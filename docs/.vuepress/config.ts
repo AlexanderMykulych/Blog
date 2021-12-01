ba458a6414051ae9f63ac77f282e1cf2bf92c53d
AlexMykulych <alexander.mykulych@gmail.com>
2021-12-01 20:08:58 +0200

    change vuepress config

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,42 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions, NavLink } from 'vuepress'
+import type { ViteBundlerOptions } from '@vuepress/bundler-vite'
+const fs = require("fs")
+const path = require("path")
+
+export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
+    lang: 'en-US',
+    title: 'AlexMyk',
+    description: 'AlexMykulych blog',
+
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+        sidebar: [
+            {
+                text: 'Posts',
+                link: '/posts/',
+                children: getSideBar("posts")
+            },
+        ]
+    },
+    bundler: '@vuepress/bundler-vite',
+    // options for vite bundler
+    bundlerConfig: {
+        // see below
+    },
+});
+
+function getSideBar(folder) {
+    const extension = [".md"];
+    
+    const files = fs
+      .readdirSync(path.join(`${__dirname}/../${folder}`))
+      .filter(
+        (item) =>
+          item.toLowerCase() != "readme.md" &&
+          fs.statSync(path.join(`${__dirname}/../${folder}`, item)).isFile() &&
+          extension.includes(path.extname(item))
+      ).map(x => `/${folder}/${x}`);
+    console.log(files);
+    return files;
+  }
